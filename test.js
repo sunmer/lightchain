fa03bd6f392f26ab39d5ebbda35d24eb88fd07ee
nima <nima.boustanian@gmail.com>
2018-05-04 14:04:30 +0200

    added network broadcasting function

--- test.js
+++ test.js
@@ -20,42 +20,41 @@ for(let i = 0; i < miners.length; i++) {
   const miner = miners[i];
   console.log(miner.name + " is mining");
 
-  miner.mineBlock(
-    transactions, 
-    function(newBlock) {
-      newBlock = new Block(
-        newBlock.previousHash, 
-        newBlock.timestamp, 
-        newBlock.transactions, 
-        newBlock.nonce, 
-        newBlock.hash
-      );
+  miner.mineBlock(transactions, broadcastToNetwork);
+}
 
-      for(let y = 0; y < miners.length; y++) {
-        if(miners[y].isValidNewBlock(newBlock)) {
-          isValidNewBlock = true;
-        }
-      }
-
-      if(isValidNewBlock && !hasBlockBeenMined) {
-        let transactionsToMake = newBlock.transactions;
-        newBlock.transactions = JSON.parse(JSON.stringify(newBlock.transactions));
-
-        for(let i = 0; i < transactionsToMake.length; i++) {
-          let transaction = transactionsToMake[i];
-          transaction.from.balance = transaction.from.balance - transaction.amount;
-          transaction.to.balance = transaction.to.balance + transaction.amount;
-        }
-        
-        blockChain.blocks.push(newBlock);
-
-        for(let i = 0; i < miners.length; i++) {
-          miners[i].blockChain = blockChain;
-        }
-
-        hasBlockBeenMined = true;
-        console.log(miner.name + " managed to mine the block");
-      }
-    }
+function broadcastToNetwork(newBlock) {
+  newBlock = new Block(
+    newBlock.previousHash, 
+    newBlock.timestamp, 
+    newBlock.transactions, 
+    newBlock.nonce, 
+    newBlock.hash
   );
+
+  for(let y = 0; y < miners.length; y++) {
+    if(miners[y].isValidNewBlock(newBlock)) {
+      isValidNewBlock = true;
+    }
+  }
+
+  if(isValidNewBlock && !hasBlockBeenMined) {
+    let transactionsToMake = newBlock.transactions;
+    newBlock.transactions = JSON.parse(JSON.stringify(newBlock.transactions));
+
+    for(let i = 0; i < transactionsToMake.length; i++) {
+      let transaction = transactionsToMake[i];
+      transaction.from.balance = transaction.from.balance - transaction.amount;
+      transaction.to.balance = transaction.to.balance + transaction.amount;
+    }
+    
+    blockChain.blocks.push(newBlock);
+
+    for(let i = 0; i < miners.length; i++) {
+      miners[i].blockChain = blockChain;
+    }
+
+    hasBlockBeenMined = true;
+    console.log(newBlock);
+  }
 }
