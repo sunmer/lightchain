d3aedb417dd6f269743728c97b8972814026ba3f
nima <nima.boustanian@gmail.com>
2018-05-07 17:57:22 +0200

    getAddressBalance is used in test

--- lightchain-test.js
+++ lightchain-test.js
@@ -1,9 +1,9 @@
-let address1 = new Address("address 1", 1);
-let address2 = new Address("address 2", 2);
+let address1 = new Address("address 1", 3);
+let address2 = new Address("address 2", 7);
 
 let transactions = [
-  new Transaction(address1, address2, 1),
-  new Transaction(address2, address1, 2)
+  new Transaction(address1, address2, 2),
+  new Transaction(address2, address1, 3)
 ];
 
 let network = new Network([
@@ -13,4 +13,8 @@ let network = new Network([
   new Miner("miner 4", new BlockChain([new Block()]))
 ]);
 
-network.mineNextBlock(transactions, () => console.log(network));
+network.mineNextBlock(transactions, () => {
+  console.log("new block was mined succcessfully");
+  console.log("Balance of address 1 is " + network.miners[2].blockChain.getAddressBalance(address1));
+  console.log("Balance of address 2 is " + network.miners[3].blockChain.getAddressBalance(address2));
+});
